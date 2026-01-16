472b12e961b16be978d321192257f889b337d62e
1124 tonju <tonju112420@gmail.com>
2026-01-16 15:48:38 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/login.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function login() {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default login
